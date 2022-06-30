b63da6f9e99a730eba7a632d7874691feee23e99
Douglas Soffi <douglas.soffi@protonmail.com>
2022-06-30 14:00:43 -0300

    organizando imports em ordem alfabética

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.http import HttpResponse
+from django.urls import path
 
 def hello(request):
     return HttpResponse("Hello World")
