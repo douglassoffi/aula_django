eaeca0aeb95a4d6509bccfff17d43f762a15c63c
Douglas Soffi <douglas.soffi@protonmail.com>
2022-08-26 11:44:44 -0300

    Adicionando receitas e categorias

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Category
+from .models import Category, Recipe
+
+@admin.register(Category)
+@admin.register(Recipe)
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
-admin.site.register(Category, CategoryAdmin)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
--- recipes/models.py
+++ recipes/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
         return self.name
     
 
-class Recpices(models.Model):
+class Recipe(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=150)
     slug = models.SlugField()
@@ -20,10 +20,13 @@ class Recpices(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y%m%d/')
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True
     )
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
-    )    
+    )
+    def __str__(self):
+        return self.title
+    
