9d3e1129827b8654bd3253d9778b4398fef57a56
Douglas Soffi <douglas.soffi@protonmail.com>
2022-09-05 15:01:53 -0300

    separando arquivos de testes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -2,19 +2,6 @@ from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes.views import *
 
-class RecipeURLsTest(TestCase):
-    def test_home_url_is_correct(self):
-        url = reverse('recipes:home')
-        self.assertEqual(url, '/')
-
-    def test_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipe/category/1/')        
-
-    def test_recipe_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipe/1/')
-
 class RecipeViewsTest(TestCase):
     def test_home_view_is_correct(self):
         view = resolve(reverse('recipes:home'))
