2f7c8ef5d4749a5725511d371e77a158a145f5b7
Douglas Soffi <douglas.soffi@protonmail.com>
2022-09-15 11:33:36 -0300

    testando model category

---
+++ recipes/tests/test_recipe_model_category.py
@@ -0,0 +1,20 @@
+from recipes.tests.test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError
+from parameterized import parameterized
+
+class CategoryModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.category = self.make_category()
+        return super().setUp()
+
+    @parameterized.expand([
+            ('name', 50)
+        ])
+    def test_field_max_length(self, field, max_length):
+            setattr(self.category, field, 'A' * (max_length + 1))
+            with self.assertRaises(ValidationError):
+                self.category.full_clean()
+
+    def test_category_representation(self):
+        name = 'name'
+        self.assertEqual(str(self.category), name)
--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -2,7 +2,7 @@ from recipes.tests.test_recipe_base import RecipeTestBase, Recipe
 from django.core.exceptions import ValidationError
 from parameterized import parameterized
 
-class RecipeModelsTest(RecipeTestBase):
+class RecipeModelTest(RecipeTestBase):
     def setUp(self) -> None:
         self.recipe = self.make_recipe()
         return super().setUp()
@@ -31,7 +31,7 @@ class RecipeModelsTest(RecipeTestBase):
             ('preparation_time_unit', 50),
             ('servings_unit', 50)
         ])
-    def test_fields_max_length(self, field, max_length):
+    def test_field_max_length(self, field, max_length):
             setattr(self.recipe, field, 'A' * (max_length + 1))
             with self.assertRaises(ValidationError):
                 self.recipe.full_clean()
@@ -47,3 +47,7 @@ class RecipeModelsTest(RecipeTestBase):
         self.assertFalse(
             recipe.is_published,
         )
+
+    def test_string_representation(self):
+        title = 'title'
+        self.assertEqual(str(self.recipe), title)
