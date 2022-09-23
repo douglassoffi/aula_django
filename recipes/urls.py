22d9db1070e87818f6a5fb85eb25853b563f475f
Douglas Soffi <douglas.soffi@protonmail.com>
2022-09-23 14:15:47 -0300

    adicionando search view e url

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,7 +5,7 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', home, name="home"),
+    path('recipe/search/', search, name="search"),
     path('recipe/category/<int:category_id>/', category, name="category"),
     path('recipe/<int:id>/', recipe, name="recipe"),
-    path('recipe/search/', lambda request: ..., name="search"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,36 +2,28 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
 
 def home(request):
-    recipes = Recipe.objects.filter(
-        is_published=True,
-        ).order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True,).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
         'title': 'Home',
     })
 
 def category(request, category_id):
-    recipes = get_list_or_404(
-        Recipe.objects.filter(
-            category__id=category_id, 
-            is_published=True,
-        ).order_by('-id')
-    )
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True,).order_by('-id'))
     return render(request, 'recipes/pages/category.html', context={
     'recipes': recipes,
     'title': f'{recipes[0].category.name} - Categoria',
     })
 
 def recipe(request, id):
-    recipe = get_object_or_404(
-        Recipe.objects.filter(
-            id=id,
-            is_published=True,
-        )
-    )
+    recipe = get_object_or_404(Recipe.objects.filter(id=id,is_published=True,))
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'title': recipe.title,
         'is_detail_page': True,
     })
     
+def search(request):
+    return render(request, 'recipes/pages/search-view.html', context={
+        'title': 'Search',
+    })
