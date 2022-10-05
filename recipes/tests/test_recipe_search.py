f922db9df03ec360585dc4ee569acc2539a794b4
Douglas Soffi <douglas.soffi@protonmail.com>
2022-10-05 14:51:25 -0300

    criando mais testes

--- recipes/tests/test_recipe_search.py
+++ recipes/tests/test_recipe_search.py
@@ -22,3 +22,28 @@ class RecipeSearchTest(RecipeTestBase):
     def test_search_term_is_escaped(self):
         response = self.client.get(reverse('recipes:search') + '?q=<test>')
         self.assertIn('&quot;&lt;test&gt;&quot;', response.content.decode('utf-8'))  
+
+    def test_search_can_find_by_title(self):
+        title1 = 'title 1'
+        title2 = 'title 2'
+
+        recipe1 = self.make_recipe(
+            slug='1', title=title1, author={'username': '1'}
+        )
+        recipe2 = self.make_recipe(
+            slug='2', title=title2, author={'username': '2'}
+        )
+
+        search_url = reverse('recipes:search')
+        response1 = self.client.get(f'{search_url}?q={title1}')
+        response2 = self.client.get(f'{search_url}?q={title2}')
+        response_both = self.client.get(f'{search_url}?q=title')
+
+        self.assertIn(recipe1, response1.context['recipes'])
+        self.assertNotIn(recipe2, response1.context['recipes'])
+
+        self.assertIn(recipe2, response2.context['recipes'])
+        self.assertNotIn(recipe1, response2.context['recipes'])
+
+        self.assertIn(recipe1, response_both.context['recipes'])
+        self.assertIn(recipe2, response_both.context['recipes'])
