8465017debf866bfbc5172ad53deae4c3811db23
Douglas Soffi <douglas.soffi@protonmail.com>
2022-10-07 15:06:56 -0300

    criando função paa paginação + testes

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -2,10 +2,19 @@ from unittest import TestCase
 from pagination import make_pagination_range
 
 class PaginationTest(TestCase):
-    def test_pagination_range_returns_pagination_tes(self):
+    def test_pagination_range_returns_pagination_range(self):
         pagination = make_pagination_range(
             page_range=list(range(1,21)),
-            qty_pages=5,
+            qty_pages=4,
             current_page=1
             )
-        self.assertEqual([0, 1, 2, 3, 4], pagination) 
+        self.assertEqual([1, 2, 3, 4], pagination) 
+
+    def test_change_range_when_current_page_is_greater_than_middle_page(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1,21)),
+            qty_pages=4,
+            current_page=5
+            )
+        self.assertEqual([4, 5, 6, 7], pagination)
+    
