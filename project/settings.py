61e3b1762f90edf0986384fb323603b8015ea264
Douglas Soffi <douglas.soffi@protonmail.com>
2022-10-17 14:47:39 -0300

    corringendo erro de digitação

--- project/settings.py
+++ project/settings.py
@@ -138,6 +138,6 @@ MESSAGE_TAGS = {
     constants.DEBUG: 'message-debug',
     constants.ERROR: 'message-error',
     constants.INFO: 'message-info',
-    constants.SUCCESS: 'message-seccess',
+    constants.SUCCESS: 'message-success',
     constants.WARNING: 'message-warning',
 }
