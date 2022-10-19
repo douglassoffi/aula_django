9e46fcebeddd976691a7af4ae99270c76f86b636
Douglas Soffi <douglas.soffi@protonmail.com>
2022-10-19 15:11:33 -0300

    criando formulário para registro de usuário

--- authors/views.py
+++ authors/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from authors.forms import UserForm
 
 def register(request):
-    return render(request, 'authors/pages/register.html')
+    if request.POST:
+        form = UserForm(request.POST)
+    else:
+        form = UserForm()
+    return render(request, 'authors/pages/register.html', context={
+        'form':form,
+        'title': 'Registre-se'
+    })
