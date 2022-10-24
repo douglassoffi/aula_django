afc57b466cfaad28b6a97fbddbf89dc7da67bd40
Douglas Soffi <douglas.soffi@protonmail.com>
2022-10-24 16:23:34 -0300

    salvando formulário se válido

--- authors/views.py
+++ authors/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from authors.forms import RegisterForm
 from django.http import Http404
+from django.contrib import messages
 
 def register(request):
     register_form_data = request.session.get('register_form_data')
@@ -13,7 +14,17 @@ def register(request):
 def register_create(request):
     if not request.POST:
         raise Http404()
+        
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Usuário criado com sucesso!')
+        del(request.session['register_form_data'])
+    
+    else:
+        messages.error(request, 'Verifique os campos!')
+
     return redirect('authors:register')
