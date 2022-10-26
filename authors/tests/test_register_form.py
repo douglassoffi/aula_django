463a6eea9cc8a3c7306434b85b83d26d3e7793cb
Douglas Soffi <douglas.soffi@protonmail.com>
2022-10-26 15:09:31 -0300

    criando teste de integração

--- authors/tests/test_register_form.py
+++ authors/tests/test_register_form.py
@@ -1,16 +1,27 @@
 from django.test import TestCase
 from authors.forms import RegisterForm
 from parameterized import parameterized
+from django.urls import reverse
 
-class RegisterFormTess(TestCase):
-
+class RegisterFormTest(TestCase):
+    def setUp(self, *args, **kwargs):
+        self.form_data = {
+            'first_name': 'first',
+            'last_name': 'last',
+            'username': 'user',
+            'email': 'email@email.com',
+            'password': 'Password123',
+            'password2': 'Password123',
+        }
+        return super().setUp(*args, **kwargs)
+ 
     @parameterized.expand([
-        ('username', '*Nome de usuário'),
-        ('password', '*Senha'),
-        ('password2', '*Confirme sua senha'),
+        ('username', 'Este campo é obrigatório.'),
+        ('password', 'Este campo é obrigatório.'),
+        ('password2', 'Este campo é obrigatório.'),
     ])
-    def test_label(self, field, label):
-        form = RegisterForm()
-        label2 = form[field].field.label
-        self.assertEqual(label, label2)
-        
+    def test_fields_cannot_be_empty(self, field, msg):
+        self.form_data[field] = ''
+        url = reverse('authors:create')
+        response = self.client.post(url, data=self.form_data, follow=True)
+        self.assertIn(msg, response.context['form'].errors.get(field))
